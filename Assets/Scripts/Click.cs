210900e1772c882f19532c9980e0375d9a2fe244
kkkkoyo <mmkrz.sg@gmail.com>
2017-08-30 02:28:50 +0900

    shader sample

---
+++ Assets/Scripts/Click.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Click : MonoBehaviour {
+
+
+	public GameObject cube;
+	public Camera maincamera;
+	int count = 1;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0)){
+
+			Vector3 position = Input.mousePosition;
+			position.z = 10;
+			Vector3 screenToWorldPointPosition = maincamera.ScreenToWorldPoint(position);
+			
+             GameObject cubeOb =  Instantiate(cube,screenToWorldPointPosition,Quaternion.Euler(45,45,45));
+
+			 cubeOb.name = count.ToString();
+			 count++;
+    	}
+
+	}
+}
---
+++ Assets/Scripts/Cube.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cube : MonoBehaviour
+{
+    public bool isRotation = true;
+    public float speed = 30;
+    private int counter = 0;
+
+    void Update ()
+    {
+        if (!isRotation)
+            return;
+        var r = transform.rotation.eulerAngles;
+        r.y += speed * Time.deltaTime;
+        transform.rotation = Quaternion.Euler (r);
+
+        counter++;
+        if(counter>30)
+        {
+            Vector3 position = Input.mousePosition;
+			position.z = 10;
+			Vector3 screenToWorldPointPosition = Camera.main.ScreenToWorldPoint(position);
+            GameObject cubez ;
+           if(GameObject.Find((int.Parse(this.name)+1).ToString())!=null)
+           {
+               cubez = GameObject.Find((int.Parse(this.name)+1).ToString());
+           
+                var moveHash = new Hashtable();
+                cubez = GameObject.Find((int.Parse(this.name)+1).ToString());
+                moveHash.Add ("position",cubez.transform.position);
+                moveHash.Add ("time", 1.0f);
+                moveHash.Add ("oncomplete", "AnimationEnd");
+                moveHash.Add ("oncompletetarget", this.gameObject);
+                iTween.MoveTo (this.gameObject, moveHash);
+           }
+        }
+    }
+
+}
